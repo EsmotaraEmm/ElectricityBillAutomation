f278269fcc68b7988b9c54ca58743c69c2ef4b77
EsmotaraEmm <esmotaraemm@iut-dhaka.edu>
2026-08-08 12:54:26 +0600

    Test formatting violation

--- ElectricityBillCalculator/Class1.cs
+++ ElectricityBillCalculator/Class1.cs
@@ -2,14 +2,14 @@
 
 public class BillCalculator
 {
-    public decimal CalculateBill(int units)
+public decimal CalculateBill(int units)
     {
-       if (units < 0)
-{
-throw new ArgumentException(
-"Electricity usage cannot be negative."
-);
-}
+        if (units < 0)
+        {
+            throw new ArgumentException(
+                "Electricity usage cannot be negative."
+            );
+        }
 
         decimal bill = 0;
 
